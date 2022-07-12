f194bd48490cf7bc1e5bca39e266c185ed95fd42
Mokshada-Jawale <104382519+Mokshada-Jawale@users.noreply.github.com>
2022-07-12 21:50:33 +0530

    Add files via upload

---
+++ practical 5/6.concatinate 2 lists.py
@@ -0,0 +1,4 @@
+li1 = list(map(int,input('Enter elements of 1st list: ').split()))
+li2 = list(map(int,input('Enter elements of 2nd list: ').split()))
+li = li1 + li2
+print('Concatination of lists: ',li)
