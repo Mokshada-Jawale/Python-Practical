ef50214510f892416c2705d62f9367e24d769d93
Mokshada-Jawale <104382519+Mokshada-Jawale@users.noreply.github.com>
2022-07-12 22:35:10 +0530

    Update 3.intersection of 2 lists.py

--- practical 6/3.intersection of 2 lists.py
+++ practical 6/3.intersection of 2 lists.py
@@ -1,15 +1,10 @@
 #Program to find intersection of 2 lists
-from sys import stdin, stdout, setrecursionlimit
-from collections import Counter
 print("Enter the elements of list A: ")
 a=list(map(int,input().split()))
 print("Enter the elements of list B: ")
 b=list(map(int,input().split()))
-A=Counter(a)
-B=Counter(b)
-print("Intersection of the given lists is: ")
-for i in A:
-    if i in B:
-        x=min(A[i],B[i])
-        for j in range(x):
-            print(i,end=' ')
+li = []
+for i in a:
+    if i in b:
+        li.append(i)
+print("Intersection of 2 lists is: ",li)
