b6d04bf85acfec4fa79b70617d625f5ab19ff2da
Mokshada-Jawale <104382519+Mokshada-Jawale@users.noreply.github.com>
2022-07-12 21:48:08 +0530

    Update 5.multiply 2 lists.py

--- practical 5/5.multiply 2 lists.py
+++ practical 5/5.multiply 2 lists.py
@@ -1,8 +1,8 @@
 li = [1,2,3,4]
 li2 = [4,5,6,7]
-print(str(li))
-print(str(li2))
+print('Print the elements of 1st list: ',str(li))
+print('Print the elements of 2nd list: ',str(li2))
 li0 = []
 for i in range(len(li)):
     li0.append(li[i]*li2[i])
-print(str(li0))
+print('List after multiplying two lists is: ',str(li0))
