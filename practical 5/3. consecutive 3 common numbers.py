22efa1b0f5c281b30dc0e275fcfdb6613a939cbb
Mokshada-Jawale <104382519+Mokshada-Jawale@users.noreply.github.com>
2022-07-12 21:53:51 +0530

    Update 3. consecutive 3 common numbers.py

--- practical 5/3. consecutive 3 common numbers.py
+++ practical 5/3. consecutive 3 common numbers.py
@@ -1,5 +1,5 @@
-li = list(map(int,input().split()))
+li = list(map(int,input('The given list is: ').split()))
 length = len(li)
 for i in range(length-2):
     if li[i]==li[i+1] and li[i+1]==li[i+2]:
-        print(li[i])
+        print('The 3 consecutive common number is: ',li[i])
