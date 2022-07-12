db133d0af8e9fdc520d25d497c27020fb66deb42
Mokshada-Jawale <104382519+Mokshada-Jawale@users.noreply.github.com>
2022-07-12 19:35:06 +0530

    Update 3.Count tuple occurance in list of tuples.py

--- pracrical 7/3.Count tuple occurance in list of tuples.py
+++ pracrical 7/3.Count tuple occurance in list of tuples.py
@@ -3,4 +3,5 @@
 li=[(1,2,3,4,5),(6,7,8,9,0),"mokshada",(6,7,8,9,0),"MOKSHADA",(1,2,3,4),"mokshada",(4,6,8,9),(6,7,8,9,0),(1,2,3,4,5)]
 x=list(set(li))
 for i in x:
-    print(i,'occurs',li.count(i),'times')
+    if type(i) == tuple:
+        print(i,'occurs',li.count(i),'times')
